d179f3bbbce1e3b37568b36d2efb86c855acec2c
Mlax Wong <mlaxwong@gmail.com>
2018-08-12 16:38:48 +0800

    setup ci integrate with yii2 migration

--- src/config/app.test.php
+++ src/config/app.test.php
@@ -1,15 +1,28 @@
 <?php
-$config = [
+$pathBase = defined('PATH_BASE') ? constant('PATH_BASE') : dirname(__DIR__, 2);
+
+return [
+    'class' => paws\console\Application::class,
+    'controllerNamespace' => 'paws\console\controllers',
+    'controllerMap' => [
+        'migrate' => [
+            'class' => paws\console\controllers\MigrateController::class,
+            'templateFile' => '@paws/db/views/migration.php',
+            'migrationNamespaces' => [
+                "paws\migrations",
+            ],
+            'migrationPath' => $pathBase . DIRECTORY_SEPARATOR . 'migrations',
+        ],
+    ],
     'components' => [
         'db' => [
             'class' => yii\db\Connection::class,
             'dsn' => 'mysql:host=localhost;port=3306;dbname=test',
             'username' => 'root',
-            'password' => '',
+            'password' => '123456',
             'tablePrefix' => '',
             'charset' => 'utf8',
             'enableSchemaCache' => false,
         ]
     ],
 ];
-return yii\helpers\ArrayHelper::merge(require (__DIR__ . DIRECTORY_SEPARATOR . 'app.php'), $config);
