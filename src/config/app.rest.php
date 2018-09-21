fb96e9d2f0b329547d7966dc65d0b43bbdf97371
Mlax Wong <mlaxwong@gmail.com>
2018-09-21 19:32:26 +0800

    working on restful api response

--- src/config/app.rest.php
+++ src/config/app.rest.php
@@ -20,6 +20,26 @@ return [
         //         }
         //     },
         // ],
+        'response' => [
+            'class' => yii\web\Response::class,
+            'on beforeSend' => function ($event) {
+                $response = $event->sender;
+                $data = $response->data;
+                $statusCode = $response->statusCode;
+                $response->data = [
+                    'success' => $response->isSuccessful,
+                    'status' => $response->statusCode,
+                ];
+                if ($response->isSuccessful) {
+                    $response->data['data'] = $data;
+                } else {
+                    $response->data['data'] = in_array($statusCode, [401, 500]) ? [] : $data;
+                    // $response->data['message'] = $data['message'] ?: [];
+                    // $response->data['code'] = $data['code'];
+                }
+                $response->statusCode = 200;
+            },
+        ],
         'request' => [
             'enableCsrfValidation' => false, 
             'parsers' => [
