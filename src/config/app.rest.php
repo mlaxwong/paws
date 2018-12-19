fb691ed145f225f1ccc85e9cac44a9ab50439ab4
inspiren <developer@inspiren.my>
2018-12-19 11:30:26 +0800

    enhance response

--- src/config/app.rest.php
+++ src/config/app.rest.php
@@ -23,21 +23,43 @@ return [
         'response' => [
             'class' => yii\web\Response::class,
             'on beforeSend' => function ($event) {
+                
                 $response = $event->sender;
                 $data = $response->data;
                 $statusCode = $response->statusCode;
-                $response->data = [
-                    'success' => $response->isSuccessful,
-                    'status' => $response->statusCode,
-                ];
-                if ($response->isSuccessful) {
-                    $response->data['data'] = $data;
-                } else {
-                    $response->data['data'] = in_array($statusCode, [404, 401, 500]) ? [] : $data;
-                    // $response->data['message'] = $data['message'] ?: [];
-                    // $response->data['code'] = $data['code'];
+
+
+                switch ($statusCode)
+                {
+                    case 200: // ok
+                    case 201: // created
+                    case 204: // deleted
+                    case 304: // no changes
+                    case 401: // auth fail
+                    case 403: // permission fail
+                    case 422: // validation fail
+                        $response->data = [
+                            'success' => $response->isSuccessful,
+                            'status' => $response->statusCode,
+                        ];
+
+                        if ($response->isSuccessful) {
+                            $response->data['data'] = $data;
+                        } else {
+                            $response->data['data'] = in_array($statusCode, [404, 401, 500]) ? [] : $data;
+                            $response->data['message'] = isset($data['message']) ? $data['message'] : [];
+                            // $response->data['code'] = $data['code'];
+                        }
+                        $response->statusCode = 200;
+                        break;
+                    case 400: // bad request
+                    case 404: // not found
+                    case 405: // method no allow
+                    case 415: // unsupport media type
+                    case 429: // too many request
+                    case 500: // internal server error
+                        break;
                 }
-                $response->statusCode = 200;
             },
         ],
         'request' => [
